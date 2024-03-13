51355df1f0cfcf0942bd45e6fdd19d5926e453ac
SPQV MF <B90FCF15A76E432D@outlook.com>
2024-03-13 18:40:34 +0800

    sync

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,44 @@
-use std::str::FromStr;
+use std::{
+    net::{Ipv4Addr, SocketAddrV4, UdpSocket},
+    str::FromStr,
+};
 
 use clap::Parser;
-use result_dyn::{msg_boxed, DynSyncError};
+use result_dyn::{msg_boxed, DynSyncError, ResultDyn};
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Hash, Parser)]
 #[command(version)]
 pub struct Cli {
-    #[arg(required = true, value_parser = <MacAddr as FromStr>::from_str)]
-    mac_addresses: Vec<MacAddr>,
+    #[arg(
+        required = true,
+        value_name = "MAC_ADDR",
+        value_parser = <MacAddr as FromStr>::from_str,
+    )]
+    mac_addr_list: Vec<MacAddr>,
 }
 
 impl Cli {
-    pub fn run(self) {
-        todo!()
+    #[allow(clippy::missing_errors_doc)]
+    pub fn run(self) -> ResultDyn<()> {
+        let socket = UdpSocket::bind(SocketAddrV4::new(Ipv4Addr::LOCALHOST, 0))?;
+        socket.connect(SocketAddrV4::new(Ipv4Addr::BROADCAST, 9))?;
+
+        for mac_addr in self.mac_addr_list {
+            socket.send(&construct_magic_packet(mac_addr))?;
+        }
+
+        return Ok(());
     }
+}
+
+#[allow(clippy::needless_pass_by_value)]
+fn construct_magic_packet(mac_addr: MacAddr) -> [u8; 6 + 6 * 16] {
+    let mut packet: Vec<u8> = Vec::new();
+    packet.extend([u8::MAX; 6]);
+    packet.extend([mac_addr.0; 16].concat());
+    return packet
+        .try_into()
+        .expect("this should be exactly the right length");
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Hash)]
